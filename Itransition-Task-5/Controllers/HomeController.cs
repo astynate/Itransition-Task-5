b2dd230afb617687b6cbb337273be2405426ca1d
astynate <sicome.a.s@gmail.com>
2024-09-19 13:15:50 +0300

    Bug fixed

--- Itransition-Task-5/Controllers/HomeController.cs
+++ Itransition-Task-5/Controllers/HomeController.cs
@@ -7,32 +7,42 @@ namespace Itransition_Task_5.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index(double error = 100, int seed = 128, string localization = "en")
+        [HttpGet]
+        public IActionResult Users(int count = 20, int page = 0, double error = 100, int seed = 128, string localization = "en")
         {
-            error = Math.Min(error, 1000);
-            seed = Math.Min(seed, 100);
+            error = Math.Max(0, Math.Min(error, 1000));
+            seed = Math.Max(0, Math.Min(seed, 1000));
 
             ViewBag.Persons = GeneratePersonsData
             (
-                20,
+                count,
+                page,
                 error,
-                seed, 
+                seed + count + page,
                 new Faker<PersonModel>(localization)
             );
 
+            return PartialView();
+        }
+
+        public IActionResult Index()
+        {
             return View();
         }
 
-        private List<PersonModel> GeneratePersonsData(int count, double error, int seed, Faker<PersonModel> fakerInstance)
+        private List<PersonModel> GeneratePersonsData(int count, int page, double error, int seed, Faker<PersonModel> fakerInstance)
         {
             var random = new Random(seed);
             var persons = new List<PersonModel>();
             
             fakerInstance.UseSeed(seed);
+            error = RoundWithProbability(error, random);
 
             var errorGenerator = new ErrorGenerator(random);
+            var prevCountUsers = page >= 2 ? (page - 1) * 10 + 20 : page == 1 ? 20 : 0;
 
             fakerInstance
+                .RuleFor(u => u.Index, (f, u) => prevCountUsers + persons.Count + 1)
                 .RuleFor(u => u.Id, (f, u) => Guid.NewGuid())
                 .RuleFor(u => u.FullName, (f, u) => errorGenerator.GenerateMistake(error, $"{f.Name.FirstName()} {f.Name.LastName()}"))
                 .RuleFor(u => u.Address, (f, u) => errorGenerator.GenerateMistake(error, $"{f.Address.City()}, {f.Address.StreetName()}"))
@@ -44,6 +54,16 @@ namespace Itransition_Task_5.Controllers
             }
 
             return persons;
+        }
+
+        static double RoundWithProbability(double num, Random random)
+        {
+            if (num % 1 == 0) { return num; }
+
+            double fractionalPart = num - Math.Floor(num);
+            double threshold = random.NextDouble();
+
+            return threshold < fractionalPart ? Math.Ceiling(num) : Math.Floor(num);
         }
     }
 }
--- Itransition-Task-5/Models/Entities/PersonModel.cs
+++ Itransition-Task-5/Models/Entities/PersonModel.cs
@@ -2,12 +2,13 @@ namespace Itransition_Task_5.Models.Entities
 {
     public record PersonModel
     (
+        int Index,
         Guid Id,
         string FullName,
         string Address,
         string PhoneNumber
     )
     {
-        public PersonModel() : this(Guid.NewGuid(), string.Empty, string.Empty, string.Empty) { }
+        public PersonModel() : this(0, Guid.NewGuid(), string.Empty, string.Empty, string.Empty) { }
     }
 }
